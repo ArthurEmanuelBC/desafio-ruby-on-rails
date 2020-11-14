bf6b246db33dbe6184ba7c7ba0ba4146976b1210
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2020-11-13 22:16:23 -0300

    seed of default cnab types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create defaults CnabTypes
+CnabType.create(number: '1', description: 'Débito', nature: :inflow)
+CnabType.create(number: '2', description: 'Boleto', nature: :outflow)
+CnabType.create(number: '3', description: 'Financiamento', nature: :outflow)
+CnabType.create(number: '4', description: 'Crédito', nature: :inflow)
+CnabType.create(number: '5', description: 'Recebimento Empréstimo', nature: :inflow)
+CnabType.create(number: '6', description: 'Vendas', nature: :inflow)
+CnabType.create(number: '7', description: 'Recebimento TED', nature: :inflow)
+CnabType.create(number: '8', description: 'Recebimento DOC', nature: :inflow)
+CnabType.create(number: '9', description: 'Aluguel', nature: :outflow)

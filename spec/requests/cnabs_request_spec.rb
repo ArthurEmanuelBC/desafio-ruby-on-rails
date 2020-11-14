e78c79a47799fdb53d4abd965ad1b932db1724d6
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2020-11-13 23:03:20 -0300

    test cnab index with store param

--- spec/requests/cnabs_request_spec.rb
+++ spec/requests/cnabs_request_spec.rb
@@ -3,13 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Cnabs', type: :request do
 
   describe 'GET index' do
-    let!(:cnab1) { create(:cnab) }
-    let!(:cnab2) { create(:cnab) }
+    let(:store) { build(:store) }
+    let(:store_id) { store.id }
+    let!(:cnab1) { create(:cnab, store: store) }
+    let!(:cnab2) { create(:cnab, store: store) }
 
-    before { get '/cnabs' }
+    before { get '/cnabs', params: { store_id: store_id } }
 
     it { expect(response).to have_http_status(:success) }
     it { expect(response.body).to eq([cnab1, cnab2]) }
+
+    context 'when not fount store' do
+      let(:store_id) { 'some-inexistent-id' }
+
+      it { expect(response).to have_http_status(:error) }
+      it { expect(response.body).to eq('Loja não encontrada') }
+    end
   end
 
   describe 'POST create' do

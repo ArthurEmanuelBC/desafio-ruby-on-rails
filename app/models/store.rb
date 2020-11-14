fd4419ff401141bfddd0cd0a096d477e391ba473
Arthur Emanuel Brandão Carvalho <arthur_aebc@hotmail.com>
2020-11-14 08:35:26 -0300

    find method and validations from store model

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,12 @@
 class Store < ApplicationRecord
   has_many :cnabs, inverse_of: :store
+
+  validates_presence_of :name, :agent
+  validates_uniqueness_of :name
+
+  def self.find_by_name_or_create(name, agent)
+    Store.find_by!(name: name)
+  rescue ActiveRecord::RecordNotFound
+    Store.create(name: name, agent: agent)
+  end
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Store, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:cnabs).inverse_of(:store) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:agent) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
+  describe "#find_by_name_or_create" do
+    let(:name) { 'some_name' }
+    let(:agent) { 'some_agent' }
+
+    it { expect { Store.find_by_name_or_create(name, agent) }.to change { Store.count }.from(0).to(1) }
+
+    context 'when founds a Store' do
+      before { create(:store, name: name, agent: agent) }
+
+      it { expect { Store.find_by_name_or_create(name, agent) }.not_to change { Store.count } }
+      it { expect(Store.find_by_name_or_create(name, agent)).to eq(Store.find_by(name: name)) }
+    end
+  end
 end
